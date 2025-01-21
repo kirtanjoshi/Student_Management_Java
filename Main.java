dbf015509853313502cc6a5a63793f8fa59d1fdf
kirtanjoshi <kirtikirtanj@gmail.com>
2025-01-21 12:12:08 +0545

    First Commit

---
+++ Main.java
@@ -0,0 +1,15 @@
+class Student{
+    privyye name;
+    int age;
+    float marks;
+
+    public Student(String name, int age, float marks){
+        this.name = name;
+        this.age = age;
+        this.marks = marks;
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+    }
+}
